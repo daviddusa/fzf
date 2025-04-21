739aac9c383ce54cce6f4bfa85906b74586051fb
David Dusa <david.dusa.hu@gmail.com>
2025-04-21 12:57:00 +0200

    shell/key-bindings.zsh: execute history command on enter immediately and select it by tab

--- shell/key-bindings.zsh
+++ shell/key-bindings.zsh
@@ -114,21 +114,32 @@ fzf-history-widget() {
   if zmodload -F zsh/parameter p:{commands,history} 2>/dev/null && (( ${+commands[perl]} )); then
     selected="$(printf '%s\t%s\000' "${(kv)history[@]}" |
       perl -0 -ne 'if (!$seen{(/^\s*[0-9]+\**\t(.*)/s, $1)}++) { s/\n/\n\t/g; print; }' |
-      FZF_DEFAULT_OPTS=$(__fzf_defaults "" "-n2..,.. --scheme=history --bind=ctrl-r:toggle-sort --wrap-sign '\t↳ ' --highlight-line ${FZF_CTRL_R_OPTS-} --query=${(qqq)LBUFFER} +m --read0") \
+      FZF_DEFAULT_OPTS=$(__fzf_defaults "" "-n2..,.. --scheme=history --bind=ctrl-r:toggle-sort --wrap-sign '\t↳ ' --highlight-line ${FZF_CTRL_R_OPTS-} --query=${(qqq)LBUFFER} --expect=enter,tab +m --read0") \
       FZF_DEFAULT_OPTS_FILE='' $(__fzfcmd))"
   else
     selected="$(fc -rl 1 | awk '{ cmd=$0; sub(/^[ \t]*[0-9]+\**[ \t]+/, "", cmd); if (!seen[cmd]++) print $0 }' |
-      FZF_DEFAULT_OPTS=$(__fzf_defaults "" "-n2..,.. --scheme=history --bind=ctrl-r:toggle-sort --wrap-sign '\t↳ ' --highlight-line ${FZF_CTRL_R_OPTS-} --query=${(qqq)LBUFFER} +m") \
+      FZF_DEFAULT_OPTS=$(__fzf_defaults "" "-n2..,.. --scheme=history --bind=ctrl-r:toggle-sort --wrap-sign '\t↳ ' --highlight-line ${FZF_CTRL_R_OPTS-} --query=${(qqq)LBUFFER} --expect=enter,tab +m") \
       FZF_DEFAULT_OPTS_FILE='' $(__fzfcmd))"
   fi
+
   local ret=$?
-  if [ -n "$selected" ]; then
-    if [[ $(awk '{print $1; exit}' <<< "$selected") =~ ^[1-9][0-9]* ]]; then
-      zle vi-fetch-history -n $MATCH
+  local key cmd
+
+  if [[ -n "$selected" ]]; then
+    key="${selected%%$'\n'*}"
+    cmd="${selected#*$'\n'}"
+
+    if [[ $cmd =~ ^[[:space:]]*[0-9]+ ]]; then
+      zle vi-fetch-history -n "${cmd%%[[:space:]]*}"
     else # selected is a custom query, not from history
-      LBUFFER="$selected"
+      LBUFFER="$cmd"
+    fi
+
+    if [[ $key == "enter" ]]; then
+      zle accept-line
     fi
   fi
+
   zle reset-prompt
   return $ret
 }
